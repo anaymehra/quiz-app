a8699a00556dd1433d30913f3f4fddedc5494ed0
Anay Mehra <anaymehra63@gmail.com>
2024-12-26 17:38:53 +0530

    Final

--- client/src/components/AuthForm.jsx
+++ client/src/components/AuthForm.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react'
 import { Eye, EyeOff, Lock, Mail, User } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const API_URL = import.meta.env.VITE_API_URL || 'http://susu'
 
 export default function AuthForm({ mode = 'login', setUser }) {
   const navigate = useNavigate()
